045d380e2fa7d6b54a94f5f9291b490b04b075a9
Tim Donoghue <tmd112593@gmail.com>
2020-03-18 18:52:09 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,4 +1,20 @@
 class Person
+  attr_accessor :partner, :name 
+  
+  def initialize(name)
+    @name = name 
 end 
 
 
+def get_married(person)
+  self.partner = person 
+  if person.class != Person 
+    raise PartnerError
+  else 
+    person.partner = self 
+  end
+end 
+
+  class PartnerError < StandardError 
+  end
+end 
