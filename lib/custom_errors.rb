e3cb45b1502c169e2e680d845832573f77cf0d81
Tim Donoghue <tmd112593@gmail.com>
2020-03-18 18:56:54 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,6 +10,9 @@ def get_married(person)
   self.partner = person 
   if person.class != Person 
     raise PartnerError
+    rescue PartnerError => error 
+        puts error.message
+      end 
   else 
     person.partner = self 
   end
